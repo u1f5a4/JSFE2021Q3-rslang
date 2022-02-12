d1b6dfdc707171f791653c6c34bb1526e72f7833
Muhammed Abdrahman <muhammed03amin.com>
2022-02-12 17:34:45 +0600

    fix: fix target language

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,9 @@ const config = {
       },
     ],
   },
+  experiments: {
+    topLevelAwait: true,
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './src/index.html'),
