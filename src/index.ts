5196693b28f9cffa49af6ecbd7d131935b51206d
Muhammed Abdrahman <muhammed03amin.com>
2022-02-12 17:33:35 +0600

    feat: add audioCall routes

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,10 @@ import BookCardView from './modules/Book/Card/BookCardView';
 import Router from './modules/Router';
 import HomeController from './modules/Home/HomeController';
 import HomeView from './modules/Home/HomeView';
-import AudioCallController from "./modules/AudioCall/AudioCallController";
-import AudioCallView from "./modules/AudioCall/AudioCallView";
+import AudioCallController from './modules/AudioCall/AudioCallController';
+import AudioCallView from './modules/AudioCall/AudioCallView';
+import AudioCallGameController from "./modules/AudioCall/AudioCallGame/AudioCallGameController";
+import AudioCallGameView from "./modules/AudioCall/AudioCallGame/AudioCallGameView";
 
 const auth = new AuthController(new AuthView(), new AppModel());
 
@@ -18,9 +20,11 @@ const bookCard = new BookCardController(new BookCardView(), new AppModel());
 book.card = bookCard;
 
 const audioCall = new AudioCallController(new AudioCallView(), new AppModel());
+const audioCallGame = new AudioCallGameController(new AudioCallGameView(), new AppModel());
+
 
 const home = new HomeController(new HomeView(), new AppModel());
-audioCall.displayPage();
+home.displayPage();
 
 const router = new Router();
 router.init();
@@ -34,4 +38,10 @@ router.add('book/4', () => bookCard.displayPage('3'));
 router.add('book/5', () => bookCard.displayPage('4'));
 router.add('book/6', () => bookCard.displayPage('5'));
 router.add('book/difficult', () => bookCard.displayPage('difficult'));
-router.add('/audioCall', () => audioCall.displayPage())
+router.add('audioCall', () => audioCall.displayPage());
+router.add('audioCall/0', () => audioCallGame.displayPage());
+router.add('audioCall/1', () => audioCallGame.displayPage());
+router.add('audioCall/2', () => audioCallGame.displayPage());
+router.add('audioCall/3', () => audioCallGame.displayPage());
+router.add('audioCall/4', () => audioCallGame.displayPage());
+router.add('audioCall/5', () => audioCallGame.displayPage());
